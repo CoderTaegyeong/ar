f3531ddaa23ebd5b1f261792d1c641d8e087d452
Taegyeong Kim <50683606+CoderTaegyeong@users.noreply.github.com>
2023-02-27 10:34:18 +0900

    AirplaneView GUI 수정

--- src/main/java/app/reserv/AirplaneView.java
+++ src/main/java/app/reserv/AirplaneView.java
@@ -24,12 +24,14 @@ import gui.table.StringTable;
 public class AirplaneView extends AppView {
 	
 	JLabel airPlanLbl;
-	StringTable jtable;
+	StringTable jtable = new StringTable();
 	JFrame frame;
 	JScrollPane sp;
 	JButton confirmBtn;
 	DefaultTableModel model;
 	Reservation reserv;
+	
+	// 작성자: 김태경(CoderTaegyeong)
 	
 	public AirplaneView(Reservation r) {
 		this.reserv = r;
@@ -39,29 +41,28 @@ public class AirplaneView extends AppView {
 	@Override
 	public void initRootPanel() {
 		rootPanel.setBackground(new Color(240, 248, 255));
-		rootPanel.setBounds(0, 0, 850, 700);
+		rootPanel.setBounds(0, 0, 678, 584);
 		rootPanel.setLayout(null);
 		
 		// 페이지 제목
 		airPlanLbl = new JLabel("항공 일정");
-		airPlanLbl.setBounds(377, 30, 96, 30);
+		airPlanLbl.setBounds(305, 30, 96, 30);
 		airPlanLbl.setFont(new Font("맑은 고딕", Font.BOLD, 22));
 		rootPanel.add(airPlanLbl);
 	
 		// 데이터 표
-		jtable = new StringTable();
-		jtable.setColumnNames("출발지", "도착지", "출발시간", "도착시간", "시작날짜", "종료날짜", "항공편명");
-		jtable.setList(DAO.sql.select("select DEPPLACE, ARRPLACE, DEPTIME, ARRTIME, STARTDATE, ENDDATE, AIRNUM from airplan"));
+		jtable.setColumns("출발지", "도착지", "출발시간", "도착시간", "시작날짜", "종료날짜", "항공편명");
+		jtable.setList(DAO.sql.select("select DEPPLACE, ARRPLACE, DEPTIME, ARRTIME, STARTDATE, ENDDATE, AIRNUM from airplan", false));
 		
 		sp = new JScrollPane(jtable);
-		sp.setBounds(140, 93, 568, 436);
+		sp.setBounds(79, 93, 568, 436);
 		jtable.setBackground(new Color(175, 238, 238));
 		jtable.setBorder(new LineBorder(new Color(0, 0, 0)));
 		rootPanel.add(sp);
 		
 		// 확인 버튼: 출발지, 도착지 고르는 페이지로 이동
 		confirmBtn = new JButton("예약하기");
-		confirmBtn.setBounds(357, 558, 136, 44);
+		confirmBtn.setBounds(280, 558, 136, 44);
 		confirmBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				reserv.openReservView();
