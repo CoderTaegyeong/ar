0b507177ddad8dacd6c51a46320c39bde64d2af6
steha2 <live9592@gmail.com>
2023-05-19 05:27:14 +0900

    0519-2

---
+++ src/main/java/com/trickle/os/cr/ArApplication.java
@@ -0,0 +1,35 @@
+package com.trickle.os.cr;
+
+import javax.swing.Timer;
+
+import com.trickle.os.cr.gui.Gui;
+import com.trickle.os.cr.test.Debug;
+
+public class ArApplication {
+	public static final String RES_PATH = "src/main/resources/";
+	public static final String IMG_PATH = RES_PATH + "images/";
+	
+	public static void main(String[] args) {
+		Gui.setLookAndFeel(Gui.NIMBUS);
+		new ArApplication().run();
+	}
+
+	//앱 추가부분
+	public void run() {
+		long start = System.currentTimeMillis();
+		Debug.sysout(getClass() +" start...");
+		
+		AppService service = AppService.instance();
+//		service.setAttr("member", new MemberDTO("dummy","1234","name1","",""));
+//		service.setAttr("id", "dummy");
+		service.start();
+
+		//앱 시작시 로그인 창을 띄운다
+		
+		//전체 동작 타이머 시작
+		new Timer(1000, e->service.update()).start();
+	
+		long end = System.currentTimeMillis();
+		Debug.sysout("Run time: "+ (end-start)+" ms ");
+	}
+}
